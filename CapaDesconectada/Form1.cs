6d915fb8ba6f12bd35da2b601527f74e55439d3f
René Lemus <126819110+ReneMenjivar@users.noreply.github.com>
2024-09-22 17:44:54 -0600

    Add files via upload
    Lo subí así porque he tenido problemas para que me reconozca el repositorio remoto en visual

---
+++ CapaDesconectada/Form1.cs
@@ -0,0 +1,45 @@
+﻿using AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaDesconectada
+{
+    public partial class Form1 : Form
+    {
+        #region No Tipado
+        private CustomerRepository customerRepository = new CustomerRepository();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnObtenerNoTipado_Click(object sender, EventArgs e)
+        {
+            GridNoTipado.DataSource = customerRepository.ObtenerTodos();
+        }
+        #endregion
+        private void btnObtenerTipado_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
